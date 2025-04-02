9ec6c411fea22f1201a134a32ee8d13ec3753baf
datnx <datnx@yopaz.vn>
2025-04-02 15:02:50 +0700

    update webshare proxy

--- server.py
+++ server.py
@@ -1,15 +1,20 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.proxies import WebshareProxyConfig
 
 app = Flask(__name__)
 CORS(app)
 
-# Proxy miễn phí (thay thế bằng proxy hợp lệ)
-proxies = {
-    "http": "http://65.49.14.150:3128",
-    "https": "http://65.49.14.150:3128"
-}
+proxy_username = "cbdljkgs"
+proxy_password = "c8yup8ionc7n"
+
+ytt_api = YouTubeTranscriptApi(
+    proxy_config=WebshareProxyConfig(
+        proxy_username=proxy_username,
+        proxy_password=proxy_password
+    )
+)
 
 @app.route('/transcript', methods=['GET'])
 def get_transcript():
@@ -18,12 +23,10 @@ def get_transcript():
         return jsonify({"error": "Missing video_id parameter"}), 400
 
     try:
-        transcript = YouTubeTranscriptApi.get_transcript(video_id, proxies=proxies)
+        transcript = ytt_api.get_transcript(video_id)
         return jsonify({"transcript": transcript})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
-
